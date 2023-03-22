f33cb712e1efeb718d06a0ac8b0c403aeb88305b
whsiq <staggsce@mail.uc.edu>
2023-03-22 19:34:21 -0400

    updated dependencies to match tutorial's

--- resources/js/game.js
+++ resources/js/game.js
@@ -12,6 +12,7 @@ Game.preload = function() {
 };
 
 Game.create = function(){
+    Game.playerMap = {};
     var map = game.add.tilemap('map');
     map.addTilesetImage('tilesheet', 'tileset');
     var layer;
@@ -19,6 +20,30 @@ Game.create = function(){
         layer = map.createLayer(i);
     }
     layer.inputEnabled = true; // Allows clicking on the map
+    Client.askNewPlayer();
+    layer.events.onInputUp.add(Game.getCoordinates, this);
+};
+
+Game.addNewPlayer = function(id,x,y) {
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
+
+Game.getCoordinates = function(layer, pointer){
+    Client.sendClick(pointer.worldX, pointer.worldY);
+};
+
+Game.movePlayer = function(id, x, y) {
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x, player.y, x, y);
+    var duration = distance * 10;
+    var tween = game.add.tween(player);
+    tween.to({x:x,y:y}, duration);
+    tween.start();
 };
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, document.getElementById('game'));
