ec3a1dc84cab8ddebc1439bb3c252f4ff5a4b8c7
whsiq <staggsce@mail.uc.edu>
2023-03-22 16:09:30 -0400

    place map info

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,20 +1,26 @@
-window.onload = function() {
 
-    config = {
-        width: 1920,
-        height: 1080,
-        backgroundColor: 0x000000,
-        scene: [Scene1]
+var Game = {};
+
+Game.init = function(){
+    game.stage.disableVisibilityChange = true;
+};
+
+Game.preload = function() {
+    game.load.tilemap('map', 'resources/assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'resources/assets/map/tilesheet.png',32,32);
+    game.load.image('sprite','resources/assets/sprites/sprite.png'); // player sprite
+};
+
+Game.create = function(){
+    var map = game.add.tilemap('map');
+    map.addTilesetImage('tilesheet', 'tileset');
+    var layer;
+    for(var i = 0; i < map.layers.length; i++) {
+        layer = map.createLayer(i);
     }
+    layer.inputEnabled = true; // Allows clicking on the map
+};
 
-    gameSettings = {
-        cellXCount: 160,
-        cellYCount: 90,
-        edgeCount: 6,
-        cells: [],
-        stagingCells: []
-
-    }
-
-    var game = new Phaser.Game(config);
-}
+var game = new Phaser.Game(800, 600, Phaser.AUTO, document.getElementById('game'));
+game.state.add('Game',Game);
+game.state.start('Game');
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use('/css',express.static(__dirname + '/resources/css'));
+app.use('/js',express.static(__dirname + '/resources/js'));
+app.use('/assets',express.static(__dirname + '/resources/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+server.listen(8081,function(){ // Listens to port 8081
+    console.log('Listening on '+server.address().port);
+});
